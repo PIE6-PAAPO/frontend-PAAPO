8e4ea5d703cb64395977396f2def4c695be1f71d
samuel Dos Santos Gonçalves da Silva <samuelsantos20021@gmail.com>
2025-04-20 16:15:25 -0300

    feat: concluir implementação da responsividade da navbar

---
+++ frontend-PAAPO/src/app/components/homepage/homepage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../layout/navbar/navbar.component';
+
+
+@Component({
+  selector: 'app-homepage',
+  imports: [NavbarComponent],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.scss'
+})
+export class HomepageComponent {
+
+}
