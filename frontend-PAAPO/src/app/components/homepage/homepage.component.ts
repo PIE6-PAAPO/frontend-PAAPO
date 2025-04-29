ddc4368614b9ff8328fd755a57ece0bfec7007e1
samuel Dos Santos Gonçalves da Silva <samuelsantos20021@gmail.com>
2025-04-28 23:32:07 -0300

    realizar ajustes no footer

--- frontend-PAAPO/src/app/components/homepage/homepage.component.ts
+++ frontend-PAAPO/src/app/components/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../layout/navbar/navbar.component';
+import { FooterComponent } from '../../layout/footer/footer.component';
 
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,FooterComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
