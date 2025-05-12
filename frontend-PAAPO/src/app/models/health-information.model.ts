a8adabf8fc12962382e145601aedab7f7072b32f
Saler C. Lourenço <salerclourenco@gmail.com>
2025-05-12 19:44:11 -0300

    cria models

---
+++ frontend-PAAPO/src/app/models/health-information.model.ts
@@ -0,0 +1,9 @@
+export interface HealthInformation {
+  weight: number;
+  height: number;
+  smoker: boolean;
+  alcohol_consumption: string;
+  physical_activity_frequency: string;
+  physical_activity_type: string;
+  user_id: string;
+}
---
+++ frontend-PAAPO/src/app/models/sequela.model.ts
@@ -0,0 +1,10 @@
+export interface Sequela {
+  restrict_mobility: boolean;
+  joint_pain: boolean;
+  shoulder_arm_pain: boolean;
+  neuromas: boolean;
+  cervical_pain: boolean;
+  back_pain: boolean;
+  swelling: boolean;
+  fatigue: boolean;
+}
