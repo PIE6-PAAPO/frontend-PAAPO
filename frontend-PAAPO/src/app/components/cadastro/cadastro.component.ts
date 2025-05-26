4c489f66d56d66abd95a2bba9458a3a299ae1865
rorxtto <pedrohenriqueroratto@gmail.com>
2025-05-26 19:43:42 -0300

    integrando com models

--- frontend-PAAPO/src/app/components/cadastro/cadastro.component.ts
+++ frontend-PAAPO/src/app/components/cadastro/cadastro.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../models/user.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [MdbFormsModule],
+  standalone: true,
+  imports: [MdbFormsModule, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+  cadastroForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.cadastroForm = this.fb.group({
+      full_name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      birthdate: ['', [Validators.required]],
+      phone_number: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      occupation: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    }, {
+      validators: this.passwordMatchValidator
+    });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+
+    if (password && confirmPassword && password.value !== confirmPassword.value) {
+      confirmPassword.setErrors({ passwordMismatch: true });
+    }
+  }
+
+  onSubmit() {
+    if (this.cadastroForm.valid) {
+      const userData: User = {
+        ...this.cadastroForm.value,
+        role: 'USER',
+        health_information: null
+      };
+      
+      console.log('Dados do usuário:', userData);
+    }
+  }
 }
