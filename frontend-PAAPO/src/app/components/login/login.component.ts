6adc7110ce3e408701dd68b029ed648b4ba82100
Saler C. Lourenço <salerclourenco@gmail.com>
2025-06-30 15:35:17 -0300

    integra front-back no login

--- frontend-PAAPO/src/app/components/login/login.component.ts
+++ frontend-PAAPO/src/app/components/login/login.component.ts
@@ -1,10 +1,42 @@
+// src/app/components/login/login.component.ts
+
 import { Component } from '@angular/core';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { LoginPayload } from '../../models/login-payload.model';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [MdbFormsModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule, MdbFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  onLogin(): void {
+    const payload: LoginPayload = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.userService.login(payload).subscribe({
+      next: (res) => {
+        localStorage.setItem('token', res.access_token);
+        this.router.navigate(['/homepage']);
+      },
+      error: (err) => {
+        this.errorMessage = 'Credenciais inválidas.';
+        console.error(err);
+      },
+    });
+  }
+}
--- frontend-PAAPO/src/app/services/user.service.ts
+++ frontend-PAAPO/src/app/services/user.service.ts
@@ -2,18 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RegisterPayload } from '../models/register-payload.model';
+import { LoginPayload } from '../models/login-payload.model';
 import { environment } from '../../environments/environment';
-
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private apiUrl = environment.apiUrl; 
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   register(user: RegisterPayload): Observable<any> {
     return this.http.post(`${this.apiUrl}/api/v1/auth/register`, user);
+  }
+
+  login(payload: LoginPayload): Observable<any> {
+    return this.http.post(`${this.apiUrl}/api/v1/auth/login`, payload);
   }
 }
