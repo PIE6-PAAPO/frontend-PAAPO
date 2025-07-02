933cbe02ac9405aa27df21e9cf63e92f63faeccf
samuel Dos Santos Gonçalves da Silva <samuelsantos20021@gmail.com>
2025-07-02 08:10:52 -0300

    modais parcialmente concluidas

--- frontend-PAAPO/src/app/app.component.ts
+++ frontend-PAAPO/src/app/app.component.ts
@@ -1,33 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatDialog } from '@angular/material/dialog';
-import { CheckinModalComponent } from '../app/components/checkin-modal/checkin-modal.component';
-import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule,],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend-PAAPO';
-    resultado: string | null = null;
-
-  constructor(private dialog: MatDialog) {}
-
-  openCheckin() {
-    const dialogRef = this.dialog.open(CheckinModalComponent, {
-      width: '300px'
-    });
-
-    dialogRef.afterClosed().subscribe((valor: number | undefined) => {
-      if (valor !== undefined) {
-        const dataHora = new Date().toISOString();
-        this.resultado = `Você escolheu: ${valor} às ${dataHora}`;
-        console.log({ valor, dataHora });
-      }
-    });
-  }
 }
